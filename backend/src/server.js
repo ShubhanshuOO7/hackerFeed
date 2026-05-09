396c751fa78e731931c2442125aefa106b1c41f1
ShubhanshuOO7 <shubhanshu5320413@gmail.com>
2026-05-09 11:15:37 +0530

    make_changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,9 @@ dotenv.config();
 import app from "./app.js";
 import connectDB from "./config/db.js";
 import { scrapeHackerNews } from "./services/scraperService.js";
+import dns from "dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 connectDB();
 
